a091f309bd9b9f87811a71261eb05320e6eeb007
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2020-11-29 15:35:46 +0530

    Arrays and Big O Notation

---
+++ DataStructures/002_Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _002_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array.
+            int[] arr = { 1, 2, 3, 4, 5 };
+            // accesing the 3 rd elemet
+            int r = arr[3];
+            Console.WriteLine(r);
+            // assigning the 4th elemnt to new value
+            arr[3] = 8;
+            r = arr[3];
+            Console.WriteLine(r);
+        }
+    }
+}
