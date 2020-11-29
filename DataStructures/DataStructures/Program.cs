b6741f19e856fbf98e8d8b588c0df6cf2e0453d9
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2020-11-29 14:44:55 +0530

    Adding datastructure definiaion

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Suganya\Github\DataStructures\DataStructures\DataStructures\Definations.txt");
+
+            // Display the file contents to the console. Variable text is a string.
+            System.Console.WriteLine(text);
+        }
+    }
+}
